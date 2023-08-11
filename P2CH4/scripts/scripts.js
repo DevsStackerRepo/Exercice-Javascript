1c4c775652bb2c561dfd00bde21ad996a49ac12f
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-11 10:56:52 +0000

    Etape 2: Separer votre code en plusieurs fichier(deplacer les listes de mots et phrases dans le fichier js config)

--- P2CH4/scripts/scripts.js
+++ P2CH4/scripts/scripts.js
@@ -1,6 +1,3 @@
-// listes de mots et phrases
-const listeMots = ["Cachalot","Pétunia","Serviette"];
-const listePhrases = ["Pas de panque !","La vie, et tout le reste", "Merci pour le poisson"];
 
 // afficher le resultat du jeu
 function afficherResultat(score,nbMotsProposes){
