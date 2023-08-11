2ec5b720ccdf5955b564ebc8779cf0625fed0db5
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-11 09:40:15 +0000

    Etape 1: Decoupez votre code en fonctions (choisirPhrasesOuMots)

--- P2CH4/scripts.js
+++ P2CH4/scripts.js
@@ -2,9 +2,21 @@
 const listeMots = ["Cachalot","Pétunia","Serviette"];
 const listePhrases = ["Pas de panque !","La vie, et tout le reste", "Merci pour le poisson"];
 
+// afficher le resultat du jeu
 function afficherResultat(score,nbMotsProposes){
     console.log("votre score est de " + score + " sur " + nbMotsProposes);
 }
 
-afficherResultat(2,3)
+// choisir entre la liste de mots ou phrases
+function choisirPhrasesOuMots(){
+    let choix = prompt("entrez ('mots' ou 'phrases')");
+
+    while(choix !== "mots" && choix !== "phrases"){
+        choix = prompt("entrez ('mots' ou 'phrases')")
+    }
+}
+
+choisirPhrasesOuMots();
+
+
 
