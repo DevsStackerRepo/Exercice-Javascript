6562267ce085ca1d28fb74fdc1cc62d37d00d36d
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-11 08:34:31 +0000

    exercie etape 2 reslue

--- P2CH3/scripts.js
+++ P2CH3/scripts.js
@@ -1,11 +1,28 @@
 const listeMots = ["Cachalot","Pétunia","Serviette"];
 
+const listePhrases = ["Pas de panique","La vie, l’univers et le reste","Merci pour le poisson"];
+
 let score = 0;
 
-for(let index = 0 ; index < listeMots.length ; index++){
-    motUtilisateur = prompt("Entrez un mot de la liste de mot " + listeMots[index]);
-    if(motUtilisateur === listeMots[index]){
-        score++
-    }
+let choisirListMotsOuPhrases = prompt("faite un choix entre ('mots' ou 'phrases' lequel choisisse vous ?)");
+
+while(choisirListMotsOuPhrases !== "mots" && choisirListMotsOuPhrases !== "phrases"){
+   choisirListMotsOuPhrases = prompt("faite un choix entre ('mots' ou 'phrases' lequel choisisse vous ?)");
 }
-console.log(score);
+if(choisirListMotsOuPhrases === "mots"){
+    for(let index = 0 ; index < listeMots.length ; index++){
+        motUtilisateur = prompt("Entrez un mot: " + listeMots[index]);
+        if(motUtilisateur === listeMots[index]){
+            score++
+        }
+    }
+    console.log("votre score est de " + score + " / " + listeMots.length);
+}else{
+    for(let index = 0 ; index < listePhrases.length ; index++){
+        motUtilisateur = prompt("Entrez une phrase: " + listePhrases[index]);
+        if(motUtilisateur === listePhrases[index]){
+            score++
+        }
+    }
+    console.log("votre score est de " + score + " / " + listePhrases.length);
+}
