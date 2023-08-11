bbd85514bc9b60f252cf4f738cc4d306d5670f1e
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-11 23:24:27 +0000

    corriger la recuperation de tous les boutons radio

---
+++ PROJET-AZERTYPE/scripts/main.js
@@ -0,0 +1,9 @@
+// lancerJeu();
+let inputEcriture = document.getElementById("inputEcriture");
+let btnValiderMot = document.getElementById("btnValiderMot");
+let zoneProposition = document.querySelector(".zoneProposition");
+let zoneScoreSpan = document.querySelector(".zoneScore span");
+let optionSource = document.querySelectorAll(".optionSource input");
+
+
+
---
+++ PROJET-AZERTYPE/scripts/scritpt.js
@@ -0,0 +1,54 @@
+
+// afficher le resultat du jeu
+function afficherResultat(score,nbMotsProposes){
+    console.log("votre score est de " + score + " sur " + nbMotsProposes);
+}
+
+// choisir entre la liste de mots ou phrases
+function choisirPhrasesOuMots(){
+    let choix = prompt("entrez ('mots' ou 'phrases')");
+
+    while(choix !== "mots" && choix !== "phrases"){
+        choix = prompt("entrez ('mots' ou 'phrases')")
+    }
+
+    return choix;
+}
+
+// lancer boucle de jeu
+function lancerBoucleDeJeu(listeProposes){
+    let score = 0;
+    for(let index = 0 ; index < listeProposes.length ; index++){
+        let motUtilisateur = prompt("Tapez ici: " + listeProposes[index]);
+        if(motUtilisateur === listeProposes[index]){
+            score++
+        }
+        
+    }
+    return score;
+}
+
+// lancer jeu
+function lancerJeu(){
+    let score = 0;
+    let nbMotsProposes = 0;
+    choisirPhrasesOuMots();
+    if(choisirPhrasesOuMots() === "mots"){
+        score = lancerBoucleDeJeu(listeMots);
+        nbMotsProposes = listeMots.length
+    }else{
+        score = lancerBoucleDeJeu(listePhrases);
+        nbMotsProposes = listePhrases.length;
+    }
+
+    afficherResultat(score,nbMotsProposes)
+
+}
+
+
+
+
+
+
+
+
