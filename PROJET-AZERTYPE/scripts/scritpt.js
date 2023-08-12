54a111aac477d7205500dbfb57167e57963075f6
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-12 14:39:43 +0000

    Commenter les fonctions avec la methode prompte et supprimer toutes les appel de fonction dans la fonction lancerJeu

--- PROJET-AZERTYPE/scripts/scritpt.js
+++ PROJET-AZERTYPE/scripts/scritpt.js
@@ -9,44 +9,32 @@ function afficherResultat(score,nbMotsProposes){
 }
 
 // choisir entre la liste de mots ou phrases
-function choisirPhrasesOuMots(){
-    let choix = prompt("entrez ('mots' ou 'phrases')");
+// function choisirPhrasesOuMots(){
+//     let choix = prompt("entrez ('mots' ou 'phrases')");
 
-    while(choix !== "mots" && choix !== "phrases"){
-        choix = prompt("entrez ('mots' ou 'phrases')")
-    }
+//     while(choix !== "mots" && choix !== "phrases"){
+//         choix = prompt("entrez ('mots' ou 'phrases')")
+//     }
 
-    return choix;
-}
+//     return choix;
+// }
 
 // lancer boucle de jeu
-function lancerBoucleDeJeu(listeProposes){
-    let score = 0;
-    for(let index = 0 ; index < listeProposes.length ; index++){
-        let motUtilisateur = prompt("Tapez ici: " + listeProposes[index]);
-        if(motUtilisateur === listeProposes[index]){
-            score++
-        }
+// function lancerBoucleDeJeu(listeProposes){
+//     let score = 0;
+//     for(let index = 0 ; index < listeProposes.length ; index++){
+//         let motUtilisateur = prompt("Tapez ici: " + listeProposes[index]);
+//         if(motUtilisateur === listeProposes[index]){
+//             score++
+//         }
         
-    }
-    return score;
-}
+//     }
+//     return score;
+// }
 
 // lancer jeu
 function lancerJeu(){
-    let score = 0;
-    let nbMotsProposes = 0;
-    choisirPhrasesOuMots();
-    if(choisirPhrasesOuMots() === "mots"){
-        score = lancerBoucleDeJeu(listeMots);
-        nbMotsProposes = listeMots.length
-    }else{
-        score = lancerBoucleDeJeu(listePhrases);
-        nbMotsProposes = listePhrases.length;
-    }
-
-    afficherResultat(score,nbMotsProposes)
-
+    
 }
 
 
