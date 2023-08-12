1320312e23777a313c953d50e7ee96da896986d1
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-12 14:46:32 +0000

    suppresion des fonctions choisirPhraseOuMot et lancerBoucleDeJeu

--- PROJET-AZERTYPE/scripts/scritpt.js
+++ PROJET-AZERTYPE/scripts/scritpt.js
@@ -5,36 +5,14 @@ function afficherResultat(score,nbMotsProposes){
     let afficherScore = `
         ${score} | ${nbMotsProposes}
     `
-    zoneScoreSpan.innerHTML = afficherScore;
+    zoneScoreSpan.innerText = afficherScore;
 }
-
-// choisir entre la liste de mots ou phrases
-// function choisirPhrasesOuMots(){
-//     let choix = prompt("entrez ('mots' ou 'phrases')");
-
-//     while(choix !== "mots" && choix !== "phrases"){
-//         choix = prompt("entrez ('mots' ou 'phrases')")
-//     }
-
-//     return choix;
-// }
-
-// lancer boucle de jeu
-// function lancerBoucleDeJeu(listeProposes){
-//     let score = 0;
-//     for(let index = 0 ; index < listeProposes.length ; index++){
-//         let motUtilisateur = prompt("Tapez ici: " + listeProposes[index]);
-//         if(motUtilisateur === listeProposes[index]){
-//             score++
-//         }
-        
-//     }
-//     return score;
-// }
 
 // lancer jeu
 function lancerJeu(){
-    
+    let score = 0;
+    let nbMotsProposes = 0;
+    afficherResultat(score,nbMotsProposes);
 }
 
 
