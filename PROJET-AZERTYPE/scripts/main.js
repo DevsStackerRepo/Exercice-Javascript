57312f73827ba942429864c0f014e78cc53331a5
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-13 13:23:50 +0000

    affichage de listeMots ou listePhrase par inputRadio

--- PROJET-AZERTYPE/scripts/main.js
+++ PROJET-AZERTYPE/scripts/main.js
@@ -3,7 +3,7 @@ let inputEcriture = document.getElementById("inputEcriture");
 let btnValiderMot = document.getElementById("btnValiderMot");
 let zoneProposition = document.querySelector(".zoneProposition");
 let zoneScoreSpan = document.querySelector(".zoneScore span");
-let optionSource = document.querySelectorAll(".optionSource input");
+
 
 
 
