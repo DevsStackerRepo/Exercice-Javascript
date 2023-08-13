dac85c55be883fdbdd42e7ca1c29b44c1522e4dd
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-13 00:01:51 +0000

    affichage mot utilisateur a recopier et score

--- PROJET-AZERTYPE/scripts/scritpt.js
+++ PROJET-AZERTYPE/scripts/scritpt.js
@@ -8,16 +8,36 @@ function afficherResultat(score,nbMotsProposes){
     zoneScoreSpan.innerText = afficherScore;
 }
 
+function afficherProposition(proposition){
+let zoneProposition = document.querySelector(".zoneProposition");
+zoneProposition.innerText = proposition;
+
+}
+
 // lancer jeu
 function lancerJeu(){
+    let score = 0;
+    let i = 0;
 let btnValiderMot = document.getElementById("btnValiderMot");
 let inputEcriture = document.getElementById("inputEcriture");
+afficherProposition(listeMots[i])
 btnValiderMot.addEventListener("click",() => {
-    console.log(`mot entre: ${inputEcriture.value}`);
+    if(inputEcriture.value === listeMots[i]){
+        score++
+    }
+    i++
+    afficherResultat(score,i);
+    inputEcriture.value = "";
+    if(listeMots[i] === undefined){
+        afficherProposition("le jeu est terminé");
+        btnValiderMot.disabled = true;
+    }else{
+
+        afficherProposition(listeMots[i])
+    }
+
 })
-    let score = 0;
-    let nbMotsProposes = 0;
-    afficherResultat(score,nbMotsProposes);
+    afficherResultat(score,i);
 }
 
 
